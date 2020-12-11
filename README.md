d7cf1e758856a7ac2a98b04f139995cdb18c5877
masato kitamura <59131332+kitamura-hub@users.noreply.github.com>
2020-12-11 19:05:13 +0900

    re Ready to develop ・re Prepared readme ・Prepared styles

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 <h3 align="center">Todo-App</h3>
 <p align="center">This Application is Todo with React Created in CodeSandbox</p>
 <img src="img/Todo-list.png">
+
+<br />
+<h2>[ replaced this repository ]( https://github.com/kitamura-hub/Todo-App-VanillaJS )  with react</h2>
--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,34 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <React.Fragment>
+      {/* 入力エリア */}
+      <div className="input-area">
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      {/* 未完了エリア */}
+      <div className="incomplete-area">
+        <p className="title">未完了のTodo</p>
+        <ul>
+          <div className="list-row">
+            <li>something Incomplete Todo</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      {/* 完了エリア */}
+      <div className="complete-area">
+        <p className="title">完了のTodo</p>
+        <ul>
+          <div className="list-row">
+            <li>something Complete Todo</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 };
